cbb234ee5ab1b4efa8615d82b26492da9ffa8aad
dhruuujs <dhrubajiban78901@gmail.com>
2025-11-10 23:00:51 +0530

    Added error handling

--- components/weatherapp.jsx
+++ components/weatherapp.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react'
+import React,{useEffect, useState,useRef} from 'react'
 import sun from "../src/assets/sun.svg"
 import rain from '../src/assets/rain.svg'
 import cloudy from '../src/assets/cloudy.svg'
@@ -9,41 +9,15 @@ import presssureImg from '../src/assets/pressure.svg'
 
 
 function WeatherApp(){
-    const [city,setCity] = useState("");
+    const [city,setCity] = useState("London");
     const [countryCode,setCountryCode] = useState("IN")
     const [weatherData,setWeatherData] = useState(false);
     const [loading,setLoading] = useState(null);
-    const [error,setError] = useState("No error");
+    const [error,setError] = useState("");
+    const inputRef= useRef();
 
     const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast';
     const api_key = "0c3f86866602c021b5df2e8ada1c1b12"
-
-
-    let date = new Date();
-    let hrs = date.getHours();
-    let min = date.getMinutes();
-
-
-{/*
-    const fetchWeather = async()=>{
-    const apiurl = `${BASE_URL}?q=${city},${countryCode}&appid=${API_KEY}`
-
-        //setLoading(true)
-        //setError("No error")
-    try{
-        const response = await fetch(apiurl)
-        const data = await response.json();
-        setWeatherData({
-            temp: data.main.temp,
-            humidity: data.main.humidity,
-            wind:data.main.wind.speed
-        });
-
-    }catch(err){
-        setError(err.message)
-        }
-    }
-*/}
 
 
 const appIcons={
@@ -65,11 +39,18 @@ const appIcons={
 
 
    const search = async (city)=>{
+        if(!city){ 
+            setLoading(false)
+            setError("Enter city name")
+            return;
+    }
+
+    setLoading(true)
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`
          try{
             const response = await fetch(url);
             const data = await response.json();
-            const icon = appIcons[data.weather[0].icon] || clear_icon
+            const icon = appIcons[data.weather[0].icon] || sun 
             setWeatherData({
                 humidity: data.main.humidity,
                 windSpeed: data.wind.speed,
@@ -81,27 +62,36 @@ const appIcons={
                 icon: icon
             });
             console.log(data);
+            setError("")
+            setLoading(false)
         }catch(err){
             setError(err);
+            setWeatherData(false)
+            setLoading(false)
         }
     }
 
 
-
-useEffect(()=>{
-    search("New York");
-},[])
+    useEffect(()=>{
+        search("London")
+    },[])
 
 
 let formatTime=()=>{ 
-    if(hrs>12 && hs<11){
-        return "PM" 
+    let date = new Date();
+    let hrs = date.getHours();
+    let min = date.getMinutes();
+
+    if(hrs>0 && hrs<11){
+        return `${hrs}:${min} AM`; 
     }else{ 
-        return "AM";} 
+        const formattedtHrs=hrs-12;
+        return `${formattedtHrs}:${min} PM`;
+    } 
 }
     return(
         <>
-        <section className="h-[10vh] w-screen bg-[#222] flex place-content-around">
+        <section className="min-h-[10vh] w-screen bg-[#222] flex place-content-around">
          <div className=" ml-4">
             <h2 className="font-[Lato] mt-4 text-3xl font-black">Balloon</h2>
         </div>
@@ -109,33 +99,37 @@ let formatTime=()=>{
         <div>              
         <input type="text" 
             id="searchCityBar"
-            placeholder="Enter city name" 
-            onChange={(e)=>{setCity(e.target.value)}}
+            placeholder="Enter city name"
+            ref={inputRef} 
             className="mt-6 h-8 w-2xs outline-0"/>
-        
         <button id="searchBtn"
         className="bg-amber-400 h-8 w-auto rounded-r-sm" 
-        onClick={void(0)} 
+        onClick={(e)=>search(inputRef.current.value)} 
         disabled={loading}>
             {loading ? "Fetching" : "Get Weather"}
         </button>
+        <div className="text-red-500">{error}</div>
         </div>
         </section>
     
     
 <div className="m-auto">    
+    {weatherData?<>
+    
+    
+
   <section id="weatherCard" className="h-[545px] w-[1026px] p-6 rounded-[30px] mt-10">
         <div className="currentTime overflow-hidden">
             <h2 className="mt-2 ml-5 font-[Lato] font-bold text-[1.5em] text-gray-950">Current Weather at 
                 <span className="font-bold"> {weatherData.location}</span>
             </h2>
-            <h3 className="ml-5 text-gray-950 font-[Lato] font-medium text-xl">{hrs+":"+min}{formatTime}</h3>
+            <h3 className="ml-5 text-gray-950 font-[Lato] font-medium text-xl">{formatTime()}</h3>
         </div>
 
         <div className="weatherInfo font-[Montserrat] mt-15 ml-10 text-gray-950 flex">
             <img src={weatherData.icon}
             className="h-32 w-32 mt-3"/>
-            <h1 className="font-extrabold ml-3 mt-7 text-8xl">{Math.ceil(weatherData.tempCelsius)}</h1>
+            <h1 className="font-extrabold ml-3 mt-7 text-8xl">{Math.ceil(weatherData.tempCelsius)}°</h1>
         
         <div className="flex flex-col">
             <h2 className="font-extrabold mt-6 ml-4 text-6xl">{weatherData.desc}</h2>
@@ -151,22 +145,22 @@ let formatTime=()=>{
             <span className="text-[#111111] font-bold">Humidity</span>
         </div>
 
-        <div className="humidityBox">
+        <div className="windspeedBox">
             <img src={windImg}/>
             <span className="font-medium text-2xl text-[#111111]">{weatherData.windSpeed}km/hr</span>
             <br/>
             <span className="text-[#111111] font-bold">Wind Speed</span>
-        
         </div>
         
-        <div className="humidityBox">
+        <div className="pressureBox">
             <img src={presssureImg} className="h-11 mt-0.5"/>
-            <span className="font-medium text-2xl text-[#111111]">{weatherData.pressure} mm/bar</span>
+            <span className="font-medium text-2xl text-[#111111]">{weatherData.pressure}mm/bar</span>
             <br/>
             <span className="text-[#111111] font-bold">Pressure</span>
         </div>
     </div>       
     </section>
+    </>:<> </> }
  </div>
 </>
     )
