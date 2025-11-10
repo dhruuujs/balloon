c893a9f55f20b1d47411cb2434d211f50cced4de
dhruuujs <dhrubajiban78901@gmail.com>
2025-11-10 10:40:44 +0530

    Minor changes

--- components/card.jsx
+++ components/card.jsx
@@ -1,28 +1,74 @@
 import sun from "../src/assets/sun.svg"
+import rain from '../src/assets/rain.svg'
+import cloudy from '../src/assets/cloudy.svg'
 
-function Card(){
+
+import humidityImg from '../src/assets/humidity.svg'
+import windImg from '../src/assets/wind.svg'
+import perciImg from '../src/assets/precipitation.svg'
+import { useState } from "react"
+
+function Card({weather}){
+
+if(!weather) return null;
+
+
+//const [] = useState()
+let date = new Date();
+let hrs = date.getHours();
+let min = date.getMinutes();
+
+let formatTime=()=>{
+    if(hrs>12){
+        return "PM"
+    }else{
+        return "AM"
+    }
+}
+
+    //const cityName = weather.name || "Unknown";
+    const description = weather.weather[0].main || "Unknown";
+    const tempCelsius = (weather.main.temp - 273.15).toFixed(1) || "Unknown"; 
+    const humidity = weather.main.humidity  || "Unknown";
+    const windSpeed = weather.wind.speed; 
+
 
 
 return(
-    <section id="weatherCard" className="h-[545px] w-[1026px] p-6 rounded-[30px] mt-10 justify-center-safe">
+    <section id="weatherCard" className="h-[545px] w-[1026px] p-6 rounded-[30px] mt-10">
         <div className="currentTime overflow-hidden">
-            <h2 className="mt-5 ml-4 font-[Lato] font-bold text-[1.5em] text-gray-950">Current Weather</h2>
-            <h3 className="ml-4 text-gray-950 font-[Lato] font-medium text-2xl">7:35 PM</h3>
+            <h2 className="mt-2 ml-5 font-[Lato] font-bold text-[1.5em] text-gray-950">Current Weather</h2>
+            <h3 className="ml-5 text-gray-950 font-[Lato] font-medium text-xl">{hrs+":"+min}{formatTime}</h3>
         </div>
 
-        <div className="weatherInfo font-[Montserrat] mt-10 ml-10 text-gray-950 flex">
+        <div className="weatherInfo font-[Montserrat] mt-15 ml-10 text-gray-950 flex">
             <img src={sun}
             className="h-32 w-32 mt-3"/>
-            <h1 className="font-extrabold ml-3 mt-6 text-8xl">19°</h1>
+            <h1 className="font-extrabold ml-3 mt-6 text-8xl">{tempCelsius}</h1>
         
         <div className="flex flex-col">
-            <h2 className="font-extrabold mt-6 ml-3 text-6xl">Clear</h2>
+            <h2 className="font-extrabold mt-6 ml-3 text-6xl">{description}</h2>
             <h2 className="text-3xl ml-3 font-semibold">Feels like 18°</h2>
         </div>
-        
-        
-        
         </div>
+   
+    <div className="extraInfo ml-5 mt-30 flex justify-evenly">
+        <div classname="humidityBox">
+            <img src={humidityImg}/>
+            <span className="font-medium text-2xl text-[#111111]">{humidity}</span>
+        </div>
+
+        <div classname="humidityBox">
+            <img src={windImg}/>
+            <span className="font-medium text-2xl text-[#111111]">{windSpeed}</span>
+        </div>
+        
+        <div classname="humidityBox">
+            <img src={perciImg}/>
+            <span className="font-medium text-2xl text-[#111111]">70%</span>
+        </div>
+    </div>
+        
     </section>
 )
 }
